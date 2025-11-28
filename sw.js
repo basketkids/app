f6ed2b766c7e69b9c98f5049939d837d4604bd77
bernat <bernatcosta@iesamachado.org>
2025-11-28 13:16:49 +0100

    feat: Add PWA support by introducing a web app manifest and service worker registration.

---
+++ sw.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = 'basketkids-v1';
+const ASSETS_TO_CACHE = [
+    './',
+    './index.html',
+    './public/index.html',
+    './css/styles.css',
+    './js/firebase-config.js',
+    './js/load-header.js',
+    './img/favicon.ico',
+    './img/logo.png'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                return cache.addAll(ASSETS_TO_CACHE);
+            })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => {
+                return response || fetch(event.request);
+            })
+    );
+});
